93ac9ea685ce486622a3bbec94d8a539a81b543f
Aurelie Bayre <a.bayre@gmail.com>
2017-09-25 17:18:26 +0200

    preparing sortedunion solution

---
+++ sorted_union/sortedunion.js
@@ -0,0 +1,20 @@
+function output ()
+{
+
+var userinput = document.getElementById("userinput");
+var input1 = userinput.elements["firstarray"].value;
+var arg1 = input1.split(",");
+var input2 = userinput.elements["secondarray"].value;
+var arg2 = input2.split(",");
+var input3 = userinput.elements["thirdarray"].value;
+var arg3 = input3.split(",");
+
+var result = "";
+
+document.getElementById("result1").innerHTML = "you gave the following arrays: ["+ input1 + "], [" + input2 + "], ["+ input3+"]";
+document.getElementById("result2").innerHTML = "The answer is: " + result ;
+  function uniteUnique(arg1, arg2, arg3)
+  {
+
+  }
+}
