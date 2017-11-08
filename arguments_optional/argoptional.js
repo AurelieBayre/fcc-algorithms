21ba03175ed1d7a6b7770982939c2be00f130229
Aurelie Bayre <a.bayre@gmail.com>
2017-11-08 16:27:26 +0100

    Update argoptional.js

--- arguments_optional/argoptional.js
+++ arguments_optional/argoptional.js
@@ -1,23 +1,26 @@
-
 function addTogether() {
   var arr = [].slice.call(arguments);
   
   if (arguments.length === 1)
    
-    {
-      if (typeof arr[0] !== "number")
-      {return undefined;}
-      return function (b) 
-      {
-      if (typeof b !== "number" )
-        {return undefined;}
+    {//case: only 1 arg is provided.
+      if (typeof arr[0] !== "number") //case: the arg is not a number.
+        {
+          return undefined;
+        }
+      //now, there's 1 arg, and it's a number, so we return a function that takes a second Arg.
+      return function (secondArg){
+      if (typeof secondArg !== "number" )
+        {
+          return undefined;
+        }
       
-        arr.push(b);
-      var num = arr.reduce(function (previousVal, currentVal) 
-                 {
-        return previousVal + currentVal;
-                  },);
-                           return num;                         
+        arr.push(secondArg);
+        
+        var num = arr.reduce(function (previousVal, currentVal){
+          return previousVal + currentVal;
+          });
+        return num;                         
     };                         
   }
   //now if there are two arguments
@@ -32,16 +35,8 @@ function addTogether() {
         var num = arr.reduce(function (previousVal, currentVal) 
                  {
         return previousVal + currentVal;
-                  },);
+                  });
         return num;
                            
-           }
-          
-                           
-                           
-        
-     
-         
-        
+     }
 }
-addTogether(2, "I dream");
